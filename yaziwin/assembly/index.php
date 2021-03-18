cb2b0f9159bb4083b3f057fbc9c07a520a2de06e
Hazar Ulaş Alçınkaya <76970911+balyedi@users.noreply.github.com>
2021-03-18 10:11:54 +0300

    Add files via upload

---
+++ yaziwin/assembly/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+	<title>x86 Assembly | Yazıwin</title>
+	<meta charset="utf-8">
+	<meta name="keywords" content="Assembly,Nasıl Yapılır,Yazılım,Yazıwin,Intel x86,NASM,emu8086">
+	<link rel="stylesheet" type="text/css" href="//yaziwin.c1.biz/style.css">
+</head>
+<body>
+	<a href="http://yaziwin.c1.biz"><img src="http://yaziwin.c1.biz/globalresimler/logo.png" width="15%" alt="logo"></a>
+	<center>
+		<h1>x86 Assembly</h1>
+		<br>
+		<h2><a href="register-listesi.html">Register listesi</a></h2>
+		<h2><a href="opcode-listesi.html">Instruction listesi</a></h2>
+	</center>
+</body>
+</html>
