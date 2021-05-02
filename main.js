3eec77ea0ae93bf267b150c1e96f94110c25c4f7
balyedi <76970911+balyedi@users.noreply.github.com>
2021-05-02 16:09:55 +0300

    Update main.js

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ var i; i = 0;
 setInterval(() => {
     const rainbowtext = document.querySelectorAll(".rainbow")
      for (var forloop = 0; forloop < rainbowtext.length; forloop++) {
-          rainbowtext[i].style.color = `rgb(${hslToRgb(i, 1, 0.5)[0]},${hslToRgb(i, 1, 0.5)[1]},${hslToRgb(i, 1, 0.5)[2]})`
+          rainbowtext[forloop].style.color = `rgb(${hslToRgb(i, 1, 0.5)[0]},${hslToRgb(i, 1, 0.5)[1]},${hslToRgb(i, 1, 0.5)[2]})`
      }
     //document.getElementById('RainbowText').style.color = `rgb(${hslToRgb(i, 1, 0.5)[0]},${hslToRgb(i, 1, 0.5)[1]},${hslToRgb(i, 1, 0.5)[2]})`
     i += 0.001
